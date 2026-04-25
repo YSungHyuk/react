abb8dbedcbe7a6c1a13fd246e7c07d626acf5130
shyun <dbstjdgur0@gmail.com>
2026-04-26 01:25:10 +0900

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import Button from "./components/ui/Button";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Button>Logout</Button>
     </>
   );
 }
