c14fb851541eae81a2ff159637db62a090cd2cb3
shyun <dbstjdgur0@gmail.com>
2026-05-03 18:36:01 +0900

    17

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import Inline from "./components/Inline";
+import "./App.css";
+import Global from "./components/Global";
 
 export default function App() {
-  const isLoggedIn = false;
-  // const h1Style = { color: "rgb(0,0,255)", fontSize: "30px" };
-  const h1Style = { color: isLoggedIn ? "red " : "blue", fontSize: "30px" };
   return (
     <>
-      <h1 style={{ color: "red" }}>Inline Style</h1>
-      <h1 style={{ color: "rgb(255, 0, 0)" }}>Inline Style</h1>
-      <h1 style={{ color: "rgba(255, 0, 0, 1)" }}>Inline Style</h1>
-      <h1 style={{ fontSize: "25px;" }}>Inline Style</h1>
-      <h1 style={h1Style}>Inline Style</h1>
-      <Inline h1Style={h1Style} />
+      <h1 className="title">App Component</h1>
+      <Global />
     </>
   );
 }
