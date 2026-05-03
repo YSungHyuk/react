7d330ea7daff629bb7f058f2afd777ee51a12536
shyun <dbstjdgur0@gmail.com>
2026-05-03 18:27:41 +0900

    16

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import RecipeList from "./components/RecipeList";
+import Inline from "./components/Inline";
 
 export default function App() {
+  const isLoggedIn = false;
+  // const h1Style = { color: "rgb(0,0,255)", fontSize: "30px" };
+  const h1Style = { color: isLoggedIn ? "red " : "blue", fontSize: "30px" };
   return (
     <>
-      <RecipeList />
+      <h1 style={{ color: "red" }}>Inline Style</h1>
+      <h1 style={{ color: "rgb(255, 0, 0)" }}>Inline Style</h1>
+      <h1 style={{ color: "rgba(255, 0, 0, 1)" }}>Inline Style</h1>
+      <h1 style={{ fontSize: "25px;" }}>Inline Style</h1>
+      <h1 style={h1Style}>Inline Style</h1>
+      <Inline h1Style={h1Style} />
     </>
   );
 }
---
+++ src/components/Inline.tsx
@@ -0,0 +1,7 @@
+export default function Inline({ h1Style }: { h1Style: React.CSSProperties }) {
+  return (
+    <>
+      <h1 style={h1Style}>Inline Component</h1>
+    </>
+  );
+}
