83d66de9175220f941f937968c5979eed52dfade
shyun <dbstjdgur0@gmail.com>
2026-05-03 18:59:22 +0900

    19

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import classNames from "classnames/bind";
 import styles from "./App.module.css";
 export default function App() {
+  const isLoggedIn = true;
+  const cx = classNames.bind(styles);
   return (
     <>
-      <h1 className={styles.title}>CSS 적용</h1>
-      <h2 className="title">CSS 적용X</h2>
+      <button className={styles.btn}>버튼</button>
+      <button className={`${styles.btn} ${styles["is-active"]}`}>버튼</button>
+      <button className={cx("btn")}>버튼</button>
+      <button className={cx("btn", "is-active")}>버튼</button>
+      <button className={cx("btn", { "is-active": isLoggedIn })}>버튼</button>
     </>
   );
 }
