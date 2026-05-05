69a45e204168a045acf4f9a2520a56d1e519b9cd
shyun <dbstjdgur0@gmail.com>
2026-05-06 01:41:15 +0900

    28

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,9 @@
-import styled, { keyframes, css } from "styled-components";
-
-const boxShadowMixin = css`
-  margin: 20px;
-  box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.5);
-`;
-
-const fadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-`;
-
-const Title = styled.h1<{ $color: string; $decoration: string }>`
-  color: ${(props) => props.$color};
-  text-decoration: ${(props) => props.$decoration};
-  animation: ${fadeIn} 2s ease-in;
-  &:hover {
-    color: pink;
-  }
-`;
-
-const BigTitle = styled(Title)`
-  font-size: ${(props) => props.theme.fontSizes.large};
-`;
-
-const Wrapper = styled.section`
-  padding: 2rem;
-  border: 1px solid red;
-`;
-
-const BlueBorderWrapper = styled(Wrapper)<{ $shadow: boolean }>`
-  border-color: ${(props) => props.theme.colors.primary};
-  ${(props) => props.$shadow && boxShadowMixin};
-`;
+import TrafficLight from "./components/TrafficLight";
 
 export default function App() {
   return (
     <>
-      <BlueBorderWrapper $shadow={true}>
-        <Title
-          $color="#00ff00"
-          $decoration="line-through"
-          as="p"
-          title="...hello..."
-          onClick={() => alert("hello!")}
-        >
-          App Component
-        </Title>
-        <BigTitle $color="#0000ff" $decoration="underline">
-          App Component
-        </BigTitle>
-      </BlueBorderWrapper>
+      <TrafficLight />
     </>
   );
 }
