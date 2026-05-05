88f0b13c1899f56db230a996da740ddb1eeeff4b
shyun <dbstjdgur0@gmail.com>
2026-05-05 21:26:18 +0900

    23

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import styled from "styled-components";
 
-const Title = styled.h1`
-  color: red;
-  text-decoration: underline;
+const Title = styled.h1<{ $color: string; $decoration: string }>`
+  color: ${(props) => props.$color};
+  text-decoration: ${(props) => props.$decoration};
 `;
 
 const Wrapper = styled.section`
   padding: 2rem;
   border: 1px solid red;
 `;
+
 export default function App() {
   return (
     <>
       <Wrapper>
-        <Title>App Component</Title>
+        <Title $color="#0000ff" $decoration="underline">
+          App Component
+        </Title>
       </Wrapper>
     </>
   );
