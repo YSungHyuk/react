a91a0dbe8851882f55c259adb9baa59b10e82c26
shyun <dbstjdgur0@gmail.com>
2026-05-07 23:41:05 +0900

    31

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ export default function App() {
     <>
       <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
         <button className="button">global</button>
-        <button className="bg-green-500 text-white py-3 px-5 rounded border-none cursor-pointer transition-colors duration-300 hover:bg-green-600">
-          tailwind
-        </button>
+        <button className="btns">tailwind</button>
       </div>
     </>
   );
