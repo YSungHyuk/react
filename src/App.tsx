1d89541b1de5a8787d9b05cdf61af10860f39c45
shyun <dbstjdgur0@gmail.com>
2026-05-13 00:14:19 +0900

    41

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import CheckBox from "./components/controlled/Checkbox";
 import Input from "./components/controlled/Input";
 
 export default function App() {
   return (
     <>
-      <Input />
+      {/* <Input /> */}
+      <CheckBox />
     </>
   );
 }
